d10ccdc7d9c9c54a93d4360acc24e9a303b365b7
Charlie01-creator <semakulacharles16@gmail.com>
2026-09-03 19:30:39 +0300

    Update App.jsx

--- Src/App.jsx
+++ Src/App.jsx
@@ -1,1 +1,41 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Programs from './pages/Programs.jsx'
+import ImpactStories from './pages/ImpactStories.jsx'
+import Donate from './pages/Donate.jsx'
+import Blog from './pages/Blog.jsx'
+import Contact from './pages/Contact.jsx'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
+export default function App() {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <ScrollToTop />
+      <Navbar />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/programs" element={<Programs />} />
+          <Route path="/stories" element={<ImpactStories />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/updates" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
 
